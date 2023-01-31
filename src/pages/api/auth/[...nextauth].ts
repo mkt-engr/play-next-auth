a8cf32e03172f9d24aa7a9259518ca9ba56775b5
Makito Mori <mkt-engr@gmail.com>
2023-01-31 22:55:18 +0900

    TwitterProvider追加

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,16 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
+import TwitterProvider from "next-auth/providers/twitter";
 
 export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!!!!!!!,
       clientSecret: process.env.GITHUB_SECRET!!!!!!,
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID!!!!!!!,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET!!!!!!,
     }),
   ],
   pages: {
